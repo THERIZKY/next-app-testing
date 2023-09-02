4c35f2cf6311e0d6191216159f6748d6799d44f1
RIZU <68154002+THERIZKY@users.noreply.github.com>
2023-09-02 22:42:14 +0700

    Berubah Posisi Navbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,6 @@
 import "@/styles/globals.css";
-import AppShell from "@/Components/Layouts/AppShell";
 import type { AppProps } from "next/app";
-import Navbar from "@/Components/Layouts/Navbar";
-import Footer from "@/Components/Layouts/Footer";
 
 export default function App({ Component, pageProps }: AppProps) {
-	return (
-		<>
-			<Navbar />
-			<Component {...pageProps} />
-			<Footer />
-		</>
-	);
+	return <Component {...pageProps} />;
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 import Snow from "@/Components/Elements/Snow";
 import Swal from "sweetalert2";
 import "animate.css";
+import Navbar from "@/Components/Layouts/Navbar";
+import Footer from "@/Components/Layouts/Footer";
 const copyText = () => {
 	const teksToCopy = "play.hariq.eu.org";
 
@@ -33,81 +35,85 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
 	return (
-		<div className="min-h-screen">
-			<section>
-				<div className="section-container relative overflow-hidden w-full h-screen">
-					<Snow />
-					<div className="content flex flex-col h-full justify-center items-center bg-transparan gap-5 z-20 ">
-						<div className="header  animate__animated animate__backInDown animate__fast">
-							<h1 className="text-5xl text- font-bold uppercase text-orange-500"></h1>
-							<img
-								src="/img/hariqmc.png"
-								alt="biglogo"
-								className="w-20rem md:w-35rem object-contain"
-							/>
+		<>
+			<Navbar />
+			<div className="min-h-screen">
+				<section>
+					<div className="section-container relative overflow-hidden w-full h-screen">
+						<Snow />
+						<div className="content flex flex-col h-full justify-center items-center bg-transparan gap-5 z-20 ">
+							<div className="header  animate__animated animate__backInDown animate__fast">
+								<h1 className="text-5xl text- font-bold uppercase text-orange-500"></h1>
+								<img
+									src="/img/hariqmc.png"
+									alt="biglogo"
+									className="w-20rem md:w-35rem object-contain"
+								/>
+							</div>
+							<div className="main text-white max-w-xxs md:max-w-2xl text-2xl animate__animated animate__bounceIn animate-delay-1s">
+								<p>
+									Come join us and embark on an adventure in
+									the mysterious world within our Minecraft
+									server. Join us now and invite your friends
+									to join us too!
+								</p>
+							</div>
+							<div className="footer flex justify-center w-44 h-12 ">
+								<button
+									onClick={() => {
+										copyText();
+									}}
+									className="bg-sky-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 w-full h-full rounded-xl"
+								>
+									Play Now
+								</button>
+							</div>
 						</div>
-						<div className="main text-white max-w-xxs md:max-w-2xl text-2xl animate__animated animate__bounceIn animate-delay-1s">
-							<p>
-								Come join us and embark on an adventure in the
-								mysterious world within our Minecraft server.
-								Join us now and invite your friends to join us
-								too!
-							</p>
-						</div>
-						<div className="footer flex justify-center w-44 h-12 ">
-							<button
-								onClick={() => {
-									copyText();
-								}}
-								className="bg-sky-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 w-full h-full rounded-xl"
-							>
-								Play Now
-							</button>
-						</div>
+						<img
+							src="/img/bg-3.jpg"
+							className="image-cover w-screen h-full absolute -z-10 object-cover top-0 left-0"
+							alt=""
+						/>
 					</div>
-					<img
-						src="/img/bg-3.jpg"
-						className="image-cover w-screen h-full absolute -z-10 object-cover top-0 left-0"
-						alt=""
-					/>
-				</div>
-			</section>
+				</section>
 
-			{/* Section kedua */}
-			<section>
-				<div className="section-container relative overflow-hidden w-full h-screen">
-					<div className="content flex flex-col h-full  items-start bg-linear-gradient-About  gap-5 z-20 pt-32 pl-20">
-						<div className="header  animate__animated animate__backInDown animate__fast">
-							<h1 className="text-5xl text- font-bold uppercase text-orange-500">
-								About
-							</h1>
-							<Image
-								width={500}
-								height={500}
-								src="/img/hariqabout.png"
-								alt="biglogo"
-								className="w-20rem object-contain"
-							/>
+				{/* Section kedua */}
+				<section>
+					<div className="section-container relative overflow-hidden w-full h-screen">
+						<div className="content flex flex-col h-full  items-start bg-linear-gradient-About  gap-5 z-20 pt-32 pl-20">
+							<div className="header  animate__animated animate__backInDown animate__fast">
+								<h1 className="text-5xl text- font-bold uppercase text-orange-500">
+									About
+								</h1>
+								<Image
+									width={500}
+									height={500}
+									src="/img/hariqabout.png"
+									alt="biglogo"
+									className="w-20rem object-contain"
+								/>
+							</div>
+							<div className="main text-white max-w-xxs md:max-w-2xl text-2xl animate__animated animate__bounceIn animate-delay-1s">
+								<p>
+									HariqMC is a server established by Faiq &
+									Fathar in 2022 and is an intermediate-level
+									Minecraft server based in Indonesia. It is
+									also the best Slimefun Survival Minecraft
+									server in Indonesian!
+								</p>
+							</div>
 						</div>
-						<div className="main text-white max-w-xxs md:max-w-2xl text-2xl animate__animated animate__bounceIn animate-delay-1s">
-							<p>
-								HariqMC is a server established by Faiq & Fathar
-								in 2022 and is an intermediate-level Minecraft
-								server based in Indonesia. It is also the best
-								Slimefun Survival Minecraft server in
-								Indonesian!
-							</p>
-						</div>
+						<Image
+							width={5000}
+							height={5000}
+							src="/img/bg-4.png"
+							className="image-cover w-screen h-full absolute -z-10 object-cover top-0 left-0"
+							alt=""
+						/>
 					</div>
-					<Image
-						width={5000}
-						height={5000}
-						src="/img/bg-4.png"
-						className="image-cover w-screen h-full absolute -z-10 object-cover top-0 left-0"
-						alt=""
-					/>
-				</div>
-			</section>
-		</div>
+				</section>
+			</div>
+			<Footer />
+		</>
 	);
 }
